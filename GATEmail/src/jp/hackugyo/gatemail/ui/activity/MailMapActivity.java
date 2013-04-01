6180c18bfadcac630c44f6f944668123eaf00dcd
hackugyo <game6000yen@gmail.com>
2013-04-01 10:39:52 +0900

    ActionBar-Sherlockの位置を固定．このプロジェクトは，[ライブラリなどのディレクトリ]/GATEMail/GATEMail のように配置する予定

--- GATEmail/src/jp/hackugyo/gatemail/ui/activity/MailMapActivity.java
+++ GATEmail/src/jp/hackugyo/gatemail/ui/activity/MailMapActivity.java
@@ -1,17 +1,12 @@
 package jp.hackugyo.gatemail.ui.activity;
 
 import jp.hackugyo.gatemail.R;
-import jp.hackugyo.gatemail.ui.view.PinchImageView;
 import jp.hackugyo.gatemail.util.LogUtils;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 public class MailMapActivity extends Activity {
-    PinchImageView mView;
-    boolean mIsZoomedMax = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,30 +25,5 @@ public class MailMapActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        mView = (PinchImageView) findViewById(R.id.image);
-        mView.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                mView.zoomMax();
-                LogUtils.i("tap");
-            }
-        });
-
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                LogUtils.i("click");
-                if (mIsZoomedMax) {
-                    mView.zoomMin();
-                    mIsZoomedMax = false;
-                } else {
-                    mView.zoomMax();
-                    mIsZoomedMax = true;
-                }
-
-            }
-        });
     }
 }
